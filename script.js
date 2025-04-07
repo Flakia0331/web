be7365f11f56c10e98f9c47a6959837c3d9357e8
Flakia0331 <flakia0331@gmail.com>
2025-04-07 20:19:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,14 @@ entry.target.style.opacity = 1;
 entry.target.style.transform = 'translateY(0)';
 }
 });
-}, {
-threshold: 0.1
-});
+}, { threshold: 0.1 });
 
 sections.forEach(section => observer.observe(section));
+
+const menuToggle = document.querySelector('.menu-toggle');
+const body = document.querySelector('body');
+
+menuToggle.addEventListener('click', () => {
+body.classList.toggle('menu-open');
+});
 });
